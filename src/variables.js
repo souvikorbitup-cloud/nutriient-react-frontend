c8cf472849b9f8c6c3b16f6747b0d33789cf67c7
Souvik Maity <souvik.orbitup@gmail.com>
2026-02-03 16:10:39 +0530

    fix(env): add env veriable

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // ====================== List of Health Supplements ======================
 export const healthSupplements = [
