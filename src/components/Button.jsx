3c7c087ba26f8c0b43ace5fad220002e85c3c374
Souvik Maity <souvik.orbitup@gmail.com>
2026-02-21 16:11:46 +0530

    fix(button): update button render logic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useQuiz } from "../context/QuizContext";
 import Preloder from "../sections/Preloder";
+import { useAuth } from "../context/AuthContext";
 
 const Button = ({
   text = "TAKE QUIZ NOW",
@@ -9,15 +10,16 @@ const Button = ({
 }) => {
   const navigate = useNavigate();
   const { completed, loading } = useQuiz();
+  const { user } = useAuth();
 
   if (loading) return <Preloder />;
 
   return (
     <button
-      onClick={() => navigate(completed ? "/recommend" : "/quiz")}
+      onClick={() => navigate(completed && user ? "/recommend" : "/quiz")}
       className={`rounded-xl uppercase bg-[linear-gradient(90deg,#E7497B_0%,#717FF3_100%)] px-6 py-3 text-sm font-semibold text-white shadow-md hover:opacity-90 transition cursor-pointer ${extraClass}`}
     >
-      {completed ? cText : text}
+      {completed && user ? cText : text}
     </button>
   );
 };
